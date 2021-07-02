ec79fd17ec30e0d6d2679b890c49f25d674cd309
Praveen <bvpraveen@gmail.com>
2021-07-02 13:47:47 +0800

    GET commit 1

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
@@ -12,6 +14,53 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("id:int")]
+        public IActionResult GetById(int id)
+        {
+            var result = _context.CelestialObjects.Where(c => c.Id == id)
+                                        .SingleOrDefault();
+
+            if(result == null)
+                return NotFound();
+
+            var objs = _context.CelestialObjects.Where(c => c.OrbitedObjectId == id);
+            foreach (var item in objs)
+            {
+                result.Satellites.Add(item);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("name")]
+        public IActionResult GetByName(string name)
+        {
+            var result = _context.CelestialObjects.Where(c => c.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                                                    .SingleOrDefault();
+
+            if (result == null)
+                return NotFound();
+
+            var objs = _context.CelestialObjects.Where(c => c.OrbitedObjectId == result.Id);
+            foreach (var item in objs)
+            {
+                result.Satellites.Add(item);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _context.CelestialObjects;
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
         }
     }
 }
